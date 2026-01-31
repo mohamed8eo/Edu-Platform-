d2ece56a1c84ffab992fd9f8683d3c17125d3684
Mohamed Elmorsy <mohamedmahomed403@gamil.com>
2026-01-31 13:56:28 +0200

    make the controller in the main path

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get } from '@nestjs/common';
 import { AllowAnonymous } from '@thallesp/nestjs-better-auth';
 import { AppService } from './app.service';
 
-@Controller('app')
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
   @Get()
